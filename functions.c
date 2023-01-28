27b326f6571bdb8e6a1bacf7dfa24b4c0eaca2d4
unknown <khalitiman17@gmail.com>
2023-01-28 12:47:05 +0300

    done

--- functions.c
+++ functions.c
@@ -1,150 +1,82 @@
 #include "main.h"
-#include <stdarg.h>
-/**
- * print_char - Prints a character
- * @types: List of arguments
- * @buffer: Handles print through a buffer array
- * @flags: Calculates active flag fields
- * @width: calculates the width
- * @precision: Specifies the precision
- * @size: specifies the size
- * Return: the characters printed
- */
- int print_char(va_list types, char buffer[], int flags, int width, int precision, int size)
-{
-	char c = va_arg(types, int);
+#include <stdio.h>
 
-	return (handle_right_char(c, buffer, flags, width, precision, size));
+/**
+ * rev_string - reverses a string in place
+ * @s: string to be reverse
+ * Return: A pointer to a character
+ */
+
+char *rev_string(char *s)
+{
+	int len;
+	int head;
+	char tmp;
+	char *dest;
+
+	for (len = 0; s[len] != '\0'; len++)
+	{}
+
+	dest = malloc(sizeof(char) * len + 1);
+	if (dest == NULL)
+		return (NULL);
+
+	_memcpy(dest, s, len);
+	for (head = 0; head < len; head++, len--)
+	{
+		tmp = dest[len - 1];
+		dest[len - 1] = dest[head];
+		dest[head] = tmp;
+	}
+	return (dest);
 }
 
 /**
- * print_string - prints a string of characters
- * @types: a list of arguments
- * @buffer: Buffer array to handle printf
- * @flags: active flags
- * @width: gets the width
- * @precision: specifies the precision
- * @size: specifies the size
- * Return: Number of characters to be printed.
+ * write_base - sends characters to be written on standard output
+ * @str: String to parse
  */
 
-int print_string(va_list types, char buffer, int flags, int width, int precision, int size)
+void write_base(char *str)
 {
-	int length = 0, 1;
-	char *str = va_arg(types, char *);
+	int i;
 
-	UNUSED(buffer);
-	UNUSED(flags);
-	UNUSED(width);
-	UNUSED(precision);
-	UNUSED(size);
-	if (str == NULL)
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+}
+
+/**
+ * base_len - Calculates the length of an octal number
+ * @num: The number for which the length is calculated
+ * @base: Base to be calculated by
+ * Return: An integer representing the length of a number
+ */
+
+unsigned int base_len(unsigned int num, int base)
+{
+	unsigned int i;
+
+	for (i = 0; num > 0; i++)
 	{
-		str = "(null)";
-		if (precision >= 6)
-			str = "      ";
+		num = num / base;
 	}
 
-	while (str[length] != '\0')
-		length++;
-	if (precission >= 0 && precision < length)
-		length = precision;
+	return (i);
+}
 
-	if (width > length)
-	{
-		if (flags & F_MINUS)
-		{
-			write(1, &str[0], length);
-			for (i = width - length; i > 0; i--)
-				write(1, "", 1);
-			write(1, &str[0], length);
-			return (width);
-		}
-	}
-	return (write(1, str, length));
+/**
+ * _memcpy - copy memory area
+ * @dest: Destination for copying
+ * @src: Source to copy from
+ * @n: The number of bytes to copy
+ * Return: The _memcpy() function returns a pointer to dest.
+ */
 
-	/**
-	 * print_int - prints an integer
-	 * @types: list of arguments
-	 * @buffer: Buffer array to handle print
-	 * @flags: calculates number of active flags
-	 * @width: gets the width
-	 * @precision: specifies precision
-	 * @size: size specifier
-	 * Return: number of char needed
-	 */
-	int print_int(va_list types, char buffer[], int flags, int width, int precision, int size)
-	{
-		int i = BUFF_SIZE - 2;
-		int is_negative = 0;
-		long int n = va_arg(types, long int);
-		unsigned long int num;
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
 
-		n = convert_size_number(n, size);
-
-		if (n== 0)
-			buffer[i--] = '0';
-
-		buffer[BUFF_SIZE - 1] = '\0';
-		num = (unsigned long int)n;
-
-		if (n < 0)
-		{
-			num = (unsigned long int)((-1) * n);
-			is_negative = 1;
-		}
-
-		while (num > 0)
-		{
-			buffer[i--] = (num % 10) + '0';
-			num /= 10;
-		}
-
-		i++;
-
-		return (write_number(is_negative, i, buffer, flags, width, precision, size));
-	}
-
-	/**
-	 * print_binary - prints an unsigned number
-	 * @types: a list of arguments
-	 * @buffer: buffer array to handle print
-	 * @flags: active flags
-	 * @width: get width
-	 * @presicion: precision specification
-	 * @size: size specifier
-	 * Return: number of characters printed
-	 */
-	int print_binary(va_list types, char buffer[], int flags, int width, int precision, int size)
-	{
-		unsigned int n, m, i, sum;
-		unsigned int a[32];
-		int count;
-
-		UNUSED(buffer);
-		UNUSED(flags);
-		UNused(width);
-		UNUSED(precision);
-		UNUSED(size);
-
-		n = va_arg(types, unsigned int);
-		m = 2147483648; /* (2 ^ 31) */
-		a[0] = n / m;
-		for (i = 1; i < 32; i++)
-		{
-			m /= 2;
-			a[i] = (n / m) % 2;
-		}
-		for (i = 0, sum = 0, count = 0; i < 32; i++)
-		{
-			sum += a[i];
-			if (sum || i == 31)
-			{
-				char z = '0' + a[i];
-
-				write(1, &z, 1);
-				count++
-			}
-		}
-		return (count);
-	}
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
